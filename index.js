0fd44f6695bc93986f8a2561a4872facb73818c9
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-09-17 12:28:18 +0600

    problem fixed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5174',
     'https://shop-fusion-server-one.vercel.app',
-    'https://shopfusion-sf.web.app/'
+    'https://shopfusion-sf.web.app'
   ],
   credentials: true
 }))
